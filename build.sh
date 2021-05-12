7475c0c32f4c6b7cb5576c86064da2c042115ab7
XiaoKui <wolfxiao@gmail.com>
2021-05-12 19:59:28 +0800

    merge master

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #/bin/bash
 
 version=1.0-$(date +%Y%m%d%H%M%S)
-sudo docker build -t tank:$version .
-image_id=$(sudo docker images | grep $version | awk '{print $3}')
-sudo docker tag $image_id registry.cn-hangzhou.aliyuncs.com/qingguatang/tank:$version
-sudo docker push registry.cn-hangzhou.aliyuncs.com/qingguatang/tank:$version
-sudo docker rmi -f $image_id
+docker build -t tank:$version .
+image_id=$(docker images | grep $version | awk '{print $3}')
+docker tag $image_id registry.cn-hangzhou.aliyuncs.com/qingguatang/tank:$version
+docker push registry.cn-hangzhou.aliyuncs.com/qingguatang/tank:$version
+docker rmi -f $image_id
