ab990ac90ccc23dae6d35d2af43be4913016261c
焦哥 <jiaoyingjun@youkeda.com>
2021-05-12 14:26:15 +0000

    Delete build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-
-version=1.0-$(date +%Y%m%d%H%M%S)
-docker build -t tank:$version .
-image_id=$(docker images | grep $version | awk '{print $3}')
-docker tag $image_id registry.cn-hangzhou.aliyuncs.com/qingguatang/tank:$version
-docker push registry.cn-hangzhou.aliyuncs.com/qingguatang/tank:$version
-docker rmi -f $image_id
